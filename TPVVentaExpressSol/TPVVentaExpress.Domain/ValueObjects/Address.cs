eec71ecc2c93c67f2f4af60de2deca57a60358fa
jair <jairbaldemar@gmail.com>
2024-05-24 16:22:22 -0600

    se agregan los value objects

---
+++ TPVVentaExpressSol/TPVVentaExpress.Domain/ValueObjects/Address.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPVVentaExpress.Domain.ValueObjects
+{
+    public class Address
+    {
+        public string Street { get; }
+        public string City { get; }
+        public string State { get; }
+        public string PostalCode { get; }
+
+        public Address(string street, string city, string state, string postalCode)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            PostalCode = postalCode;
+        }
+
+        // Equals and GetHashCode methods
+    }
+
+}
---
+++ TPVVentaExpressSol/TPVVentaExpress.Domain/ValueObjects/ProductCode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPVVentaExpress.Domain.ValueObjects
+{
+    public class ProductCode
+    {
+        public string Code { get; }
+
+        public ProductCode(string code)
+        {
+            Code = code;
+        }
+
+        // Equals and GetHashCode methods
+    }
+
+}
