15761131a90bc200247637504c48788303ec98cf
jair <jairbaldemar@gmail.com>
2024-05-24 16:12:16 -0600

    se agregan las entidades al proyecto

---
+++ TPVVentaExpressSol/TPVVentaExpress.Domain/Entities/Sale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPVVentaExpress.Domain.Entities
+{
+    public class Sale
+    {
+        public int SaleId { get; set; }
+        public DateTime Date { get; set; }
+        public int CustomerId { get; set; }
+        public decimal Total { get; set; }
+
+        // Lista de detalles de la venta
+        public List<SaleDetail> SaleDetails { get; set; }
+
+        // Relaciones de navegación
+        public Customer Customer { get; set; }
+    }
+
+
+}
